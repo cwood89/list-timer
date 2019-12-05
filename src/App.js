460382bf109847d4e4be73b35fd2458690978ed4
woodtop <cwood89@mac.com>
2019-12-05 14:15:24 -0500

    basic todo setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import TodoList from './TodoList'
+class App extends Component {
+  constructor() {
+    super()
+    this.inputElement = React.createRef()
+    this.state = {
+      items: [],
+      currentItem: { text: '', key: '' },
+    }
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  handleInput = e => {
+    const itemText = e.target.value
+    const currentItem = { text: itemText, key: Date.now() }
+    this.setState({
+      currentItem,
+    })
+  }
+  addItem = e => {
+    e.preventDefault();
+    const newItem = this.state.currentItem
+    if (newItem.text !== '') {
+      console.log(newItem)
+      const items = [...this.state.items, newItem]
+      this.setState({
+        items: items,
+        currentItem: { text: '', key: '' },
+      })
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <TodoList
+          inputElement={this.inputElement}
+          addItem={this.addItem}
+          handleInput={this.handleInput}
+          currentItem={this.state.currentItem} />
+      </div>
+    )
+  }
 }
-
-export default App;
+export default App
